ec25a04a92624361aed095dfddcda6bf7b4cfb18
araae <araliae@icloud.com>
2026-05-27 10:47:48 -0700

    updated

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-// import express and employees router
 import express from "express";
 import employeesRouter from "#api/employees";
 
-// create the express app
-// export the app
+//create the express app and export
 const app = express();
 export default app;
 
-// parse incoming JSON request bodies
 app.use(express.json());
 
-// send greeting message back to the client
+//send greeting message
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-// use the employees router for all /employees routes
+//use the employees router for all employees routes
 app.use("/employees", employeesRouter);
 
-// catch all uncaught errors and send a 500 status
+//catch all uncaught errors and send a 500 status
 app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
